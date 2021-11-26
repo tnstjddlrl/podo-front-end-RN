9b4fe8e57f24fc4af6a40696edb2da4be1b257e4
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-11-26 18:59:20 +0900

    로그인시 id token 어싱크 저장코드 작성중

--- screens/LoggedOut/LoginPodo.js
+++ screens/LoggedOut/LoginPodo.js
@@ -6,6 +6,8 @@ import { fonts } from "../../fonts";
 import { Input, PurpleBtn } from "../../components/share";
 import { Alert, TouchableOpacity } from "react-native";
 import axios from 'axios';
+import { useRecoilState } from 'recoil';
+import { AsyncsetUserId, AsyncsetUserToken, AtomUserId, AtomUserToken, setUserId } from "../../atom/atom";
 
 
 const TextBtnWrapper = styled.View`
@@ -20,22 +22,7 @@ const TextBtn = styled.Text`
   color: ${colors.gray};
 `;
 
-// 로그인 post 초안!
-function EmailLoginPost(email, pwd) {
-  axios.post('https://softer104.cafe24.com/Open/Login', {
-    mb_email: email,
-    mb_password: pwd
-  }).then((res) => {
-    console.log(res.data);
-  }).catch((error) => {
-    if (error.response) {
-      console.log(error.response.data.error);
-      Alert.alert(error.response.data.error);
-    } else if (error.request) {
-      console.log(error.request);
-    }
-  })
-}
+
 
 const LoginPodo = ({ navigation }) => {
   const goFindAccount = () => navigation.navigate("FindNav");
@@ -44,6 +31,33 @@ const LoginPodo = ({ navigation }) => {
 
   const [mb_email, setMb_email] = useState('')
   const [mb_pwd, setMb_pwd] = useState('')
+
+  const [atUserId, setAtUserId] = useRecoilState(AtomUserId)
+  const [atUserToken, setAtUserToken] = useRecoilState(AtomUserToken)
+
+
+  // 어싱크에 유저아이디랑 토큰 값 넣어놔야함. 지금 테스트 로그인이 안됨
+  // AsyncsetUserId()
+  // AsyncsetUserToken()
+
+
+
+  // 로그인 post 초안!
+  function EmailLoginPost(email, pwd) {
+    axios.post('https://softer104.cafe24.com/Open/Login', {
+      mb_email: email,
+      mb_password: pwd
+    }).then((res) => {
+      console.log(res.data);
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        Alert.alert(error.response.data.error);
+      } else if (error.request) {
+        console.log(error.request);
+      }
+    })
+  }
 
 
   return (
