4af9ecf3f70e91564ff04447c9dfdffb2915b1ad
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-11-26 18:58:56 +0900

    async 함수들 이름변경

--- atom/atom.js
+++ atom/atom.js
@@ -2,8 +2,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
     atom,
 } from 'recoil';
+// import { useRecoilState } from 'recoil';
 
-export const getIsFirst = async () => {
+
+//최초 진입 확인/////////////////////////////////////////////
+export const AsyncgetIsFirst = async () => {
     try {
         const value = await AsyncStorage.getItem('@is_first')
         console.log(value)
@@ -20,15 +23,75 @@ export const getIsFirst = async () => {
     }
 }
 
-export const setIsFirst = async (value) => {
+export const AsyncsetIsFirst = async (value) => {
     try {
         await AsyncStorage.setItem('@is_first', value)
     } catch (e) {
         console.log(e)
     }
 }
+/////////////////////////////////////////////////////////////
+
+//아이디 및 로그인 여부 가져오기////////////////////////////////
+export const AsyncgetUserId = async () => {
+    try {
+        const value = await AsyncStorage.getItem('@user_id')
+        console.log(value)
+
+        if (value !== null) {
+            console.log('false')
+            return 'guest'
+        } else {
+            console.log('true')
+            return value
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const AsyncsetUserId = async (value) => {
+    try {
+        await AsyncStorage.setItem('@user_id', value)
+    } catch (e) {
+        console.log(e)
+    }
+}
 
 export const AtomUserId = atom({
-    key: 'AtomUserId', // unique ID (with respect to other atoms/selectors)
-    default: '', // default value (aka initial value)
+    key: 'AtomUserId',
+    default: AsyncgetUserId(),
 });
+/////////////////////////////////////////////////////////////////
+
+//로그인 토큰 값 저장 및 불러오기/////////////////////////////////
+export const AsyncgetUserToken = async () => {
+    try {
+        const value = await AsyncStorage.getItem('@user_token')
+        console.log(value)
+
+        if (value !== null) {
+            console.log('false')
+            return 'guestToken'
+        } else {
+            console.log('true')
+            return value
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const AsyncsetUserToken = async (value) => {
+    try {
+        await AsyncStorage.setItem('@user_token', value)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const AtomUserToken = atom({
+    key: 'AtomUserToken',
+    default: AsyncgetUserToken(),
+});
+///////////////////////////////////////////////////////////////////
