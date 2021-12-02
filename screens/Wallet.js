48786704ecf5f27137e83b3d6c45c33ad7522b74
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-12-02 12:25:16 +0900

    지갑 수정중

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
+import { useRecoilState } from "recoil";
 import styled, { css } from "styled-components/native";
+import { AtomUserId, AtomUserLevel, AtomUserToken } from "../atom/atom";
 import { colors } from "../colors";
 import LayOut from "../components/LayOut";
 import { PurpleBtn } from "../components/share";
@@ -117,20 +119,82 @@ const zeroPlus = (num) => {
   }
 };
 
+// {
+//   "created_at": "2021-12-02 10:14:05",
+//   "deleted_at": null,
+//   "mb_agree_email": "N",
+//   "mb_alarm": "1",
+//   "mb_email": "sasaa3865@naver.com",
+//   "mb_level": 1,
+//   "mb_nick": "",
+//   "mb_no": "10",
+//   "mb_phone": "010-7615-3865",
+//   "mb_podo": "0",
+//   "mb_sns_id": null,
+//   "mb_state": "1",
+//   "mb_token": "",
+//   "mb_wbtc": "0",
+//   "podo": "0",
+//   "podo_date": "2021-12-02 11:21:55",
+//   "podo_kr": 0,
+//   "require_podo": 2000000,
+//   "updated_at": "2021-12-02 11:21:07",
+//   "wbtc_date": "2021-12-02 11:21:55",
+//   "wbtc_kr": 0,
+// }
+
 const Wallet = ({ navigation }) => {
   const gaugePercent = 50;
   const [name, setName] = useState("게스트");
+
+  const [atUserId, setAtUserId] = useRecoilState(AtomUserId)
+  const [atUserToken, setAtUserToken] = useRecoilState(AtomUserToken)
+
+  const [atMemberLevel, setAtMemberLevel] = useRecoilState(AtomUserLevel)
+
+
   useEffect(() => {
     getData("randomName").then((name) => setName(name));
+    console.log('토큰 : ' + atUserToken);
+    MyInformLoadAxios()
+    // TokenRefreshAxios()
   }, []);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      MyInformLoadAxios()
+
+    });
+
+    return unsubscribe;
+  }, [navigation]);
 
   function MyInformLoadAxios(params) {
     axios.get('https://softer104.cafe24.com/V1/Member/@me', {
-      headers: { Token }
+      headers: {
+        Authorization: `Bearer ${atUserToken}`
+      }
+    }).then((res) => {
+      console.log(res.data.data);
+    }).catch((error) => {
+      console.log(error);
     })
   }
 
-
+  function TokenRefreshAxios(params) {
+    axios.get('https://softer104.cafe24.com/Open/Login/Refresh', {
+      headers: {
+        Authorization: `Bearer ${atUserToken}`
+      },
+      params: {
+        refresh_token: atUserToken
+      }
+    }).then((res) => {
+      console.log(res.data);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
 
   const renderItem = ({ item }) => {
     return (
@@ -181,7 +245,7 @@ const Wallet = ({ navigation }) => {
           ListHeaderComponent={
             <>
               <LevelSection>
-                <LevelText isTitle>{name}님, Level 1 에요!</LevelText>
+                <LevelText isTitle>{atUserId}님, Level 1 에요!</LevelText>
                 <Gauge>
                   <GaugeStatus percent={gaugePercent} />
                 </Gauge>
