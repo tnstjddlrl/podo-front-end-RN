39001fc8b6f6b8c9871fad714616c36695e79c19
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-12-10 14:02:40 +0900

    스테이킹 취소 완료

--- screens/LoggedOut/CancelStacking.js
+++ screens/LoggedOut/CancelStacking.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
-import { ScrollView } from "react-native";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Alert, ScrollView } from "react-native";
+import { useRecoilState } from "recoil";
 import styled, { css } from "styled-components/native";
+import { AtomUserToken } from "../../atom/atom";
 import { colors } from "../../colors";
 import LayOut from "../../components/LayOut";
 import { CancelBtn, PurpleBtn } from "../../components/share";
@@ -136,23 +139,78 @@ const sampleHistory = [
 const CancelStacking = ({ navigation }) => {
   const [seeAll, setSeeAll] = useState(false);
   // const [selectedList, setSelectedList] = useState([]);
-  const selectedList = null;
+  const [selectedList, setSelectedList] = useState([])
 
-  const onClickSetSeeAll = () => {
-    setSeeAll(true);
+  const [atUserToken, setAtUserToken] = useRecoilState(AtomUserToken) //유저 토큰
+  const [selectNum, setSelectNum] = useState('')
+
+  useEffect(() => {
+    StakingHistoryLoadAxios()
+  }, [])
+
+  function StakingHistoryLoadAxios(params) { //스테이킹 신청내역
+    axios.get('https://softer104.cafe24.com/V1/Podo/StakeList', {
+      headers: {
+        Authorization: `Bearer ${atUserToken}`
+      },
+      params: {
+        limit: 100,
+        offset: 0,
+      }
+    }).then((res) => {
+      console.log('스테이킹 내역:')
+      console.log(res.data);
+      if (res.data.msg === 'success') {
+        setSelectedList(res.data.list)
+      }
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        // Alert.alert(error.response.data.error);
+      } else if (error.request) {
+        console.log(error.request);
+      }
+    })
+  }
+
+  function CancelStakingAxios(params) { //스테이킹 신청내역
+    axios.get('https://softer104.cafe24.com/V1/Podo/StakeCancel', {
+      headers: {
+        Authorization: `Bearer ${atUserToken}`
+      },
+      params: {
+        num: selectNum,
+      }
+    }).then((res) => {
+      console.log(res.data);
+      if (res.data.msg === 'success') {
+        Alert.alert('완료되었습니다!', '', [
+          { text: "OK", onPress: () => navigation.navigate("PocketStacking") }
+        ])
+      }
+    }).catch((error) => {
+      Alert.alert('오류가 발생하였습니다.', '나중에 다시 시도해주세요.', [
+        { text: "OK", onPress: () => navigation.navigate("PocketStacking") }
+      ])
+      if (error.response) {
+        console.log(error.response.data);
+        // Alert.alert(error.response.data.error);
+      } else if (error.request) {
+        console.log(error.request);
+      }
+    })
+  }
+
+
+
+
+  const onClickAddListToggle = (num) => {
+    console.log(num)
+    setSelectNum(num)
+
   };
 
-  const onClickAddListToggle = (index) => {
-    if (
-      selectedList.filter((item) => item === sampleHistory[index]).length === 0
-    ) {
-      setSelectedList(selectedList.concat(sampleHistory[index]));
-    } else {
-      setSelectedList(
-        selectedList.filter((item) => item !== sampleHistory[index])
-      );
-    }
-  };
+
   return (
     <LayOut paddingTop={96}>
       <TextWrapper>
@@ -165,51 +223,27 @@ const CancelStacking = ({ navigation }) => {
           아직 신청하실 스테이킹 이력이 없습니다.
         </HistoryText>
       )}
-      {selectedList && !seeAll && (
-        <>
-          <HistoryBox
-            onPress={() => onClickAddListToggle(0)}
-            selected={
-              selectedList.filter((list) => list === sampleHistory[0])
-                .length !== 0
-            }
-          >
-            <HistoryTextBlock>
-              <HistoryText date>{sampleHistory[0].date}</HistoryText>
-              <HistoryText quantity>{sampleHistory[0].quantity}</HistoryText>
-            </HistoryTextBlock>
-            <HistoryStatus>
-              <HistoryStatusText>출금신청</HistoryStatusText>
-            </HistoryStatus>
-          </HistoryBox>
-          <SeeAllBtn onPress={() => onClickSetSeeAll()}>
-            <SeeAllBtnText>+ 신청이력 더보기</SeeAllBtnText>
-          </SeeAllBtn>
-        </>
-      )}
-      {selectedList && seeAll && (
-        <ScrollView>
-          {sampleHistory.map((item, index) => {
-            return (
-              <HistoryBox
-                key={index}
-                onPress={() => onClickAddListToggle(index)}
-                selected={
-                  selectedList.filter((list) => list === item).length !== 0
-                }
-              >
-                <HistoryTextBlock>
-                  <HistoryText date>{item.date}</HistoryText>
-                  <HistoryText quantity>{item.quantity}</HistoryText>
-                </HistoryTextBlock>
-                <HistoryStatus>
-                  <HistoryStatusText>출금신청</HistoryStatusText>
-                </HistoryStatus>
-              </HistoryBox>
-            );
-          })}
-        </ScrollView>
-      )}
+      <ScrollView>
+        {selectedList.map((item, index) => {
+          return (
+            <HistoryBox
+              key={index}
+              onPress={() => onClickAddListToggle(item.num)}
+              selected={
+                (item.num == selectNum)
+              }
+            >
+              <HistoryTextBlock>
+                <HistoryText date>{item.entrance_date}</HistoryText>
+                <HistoryText quantity>{item.podo}</HistoryText>
+              </HistoryTextBlock>
+              <HistoryStatus>
+                <HistoryStatusText>출금신청</HistoryStatusText>
+              </HistoryStatus>
+            </HistoryBox>
+          );
+        })}
+      </ScrollView>
       <BtnWrapper absolute={seeAll}>
         <CancelBtn
           width={"49%"}
@@ -219,7 +253,10 @@ const CancelStacking = ({ navigation }) => {
         <PurpleBtn
           width={"49%"}
           text={"확인"}
-          onPress={() => navigation.navigate("PocketStacking")}
+          onPress={() => {
+            CancelStakingAxios()
+            // navigation.navigate("PocketStacking")
+          }}
         />
       </BtnWrapper>
     </LayOut>
